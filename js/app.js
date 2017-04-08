61e2c10b5618baa3d8b85950ea9ad8844cfb4751
IncredibleQuark <lkupinski@gmail.com>
2017-04-08 23:30:06 +0200

    Initial commit. Class book created

---
+++ js/app.js
@@ -0,0 +1,105 @@
+$(function () {
+
+    var divBooks = $('div#books');
+    //pobieramy ksiazki po zaladowaniu strony
+    $.ajax({
+        url: 'api/books.php',
+        dataType: 'json'
+    }).done(function (bookList) {
+        //bookList - tablica obiektow ksiazek w json
+        bookList.forEach(function (singleBookJson) {
+            var singleBook = JSON.parse(singleBookJson);
+            //tworzymy element li z nową książk
+            var newLi = $('<div data-id="' + singleBook.id + '"><span class="bookTitle">' + singleBook.title + '</span><div class="bookDescription" style="margin-top:20px; font-weight: bold; color: blue; font-size: 13px;"></div></div><hr>');
+            //dodajemy element do listy
+            divBooks.append(newLi);
+        });
+    }).fail(function () {
+        console.log('Error');
+    });
+
+    //dodajemy event na tytul książki
+
+    //sposob na dodawanie eventow do elementow ladowanych dynamicznie
+    //event zakladamy na dowolnego przodka NIE ZALADOWANEGO DYNAMICZNIE
+    //jako drugi argument metody on() podajemy SELEKTOR (string) 
+    //elementu dynamicznie ladowanego na jakim event nas interesuje
+    //$(this) to element z drugiego argumentu
+    divBooks.on('click', 'span.bookTitle', function () {
+        //pobieramy id książki z datasetu
+        var span = $(this);
+        var bookId = span.parent().data('id');
+        //pobieramy opis książki
+
+        $.ajax({
+            url: 'api/books.php?id=' + bookId,
+            dataType: 'json'
+        }).done(function (bookList) {
+            //bookList - tablica obiektow ksiazek w json
+            //w tym wypadku tablica 1 elementowa
+            //poneiwaz pobieramy 1 ksiązke po id
+            var singleBook = JSON.parse(bookList[0]);
+            span.next().text(singleBook.description);
+        }).fail(function () {
+            console.log('Error');
+        });
+    });
+
+    //DODAWANIE KSIĄŻKI
+
+    var addBook = $('#addBook');
+    addBook.on('click', function (e) {
+        e.preventDefault();//KONIECZNE - poniewaz wysyłamy ajaxem
+        //pobieramy sobie dane z formularza
+        var form = $(this).parent();//zapisuje do zmiennej formularz
+
+        var author = form.find('input[name=author]').val();
+        var title = form.find('input[name=title]').val();
+        var description = form.find('textarea[name=description]').val();
+
+        //tworzymy obiekt z danymi do wysłania
+        var sendObj = {};
+        sendObj.author = author;
+        sendObj.title = title;
+        sendObj.description = description;
+        //wysylamy żądanie ajax o dodanie książki
+        $.ajax({
+            url: 'api/books.php', //adres na jaki wysyłamy
+            dataType: 'json', //typ danych ZWRACANYCH
+            data: sendObj, //obiekt z danymi
+            type: 'POST'//dodajemy więc POST (REST)
+        }).done(function (bookList) {
+            var singleBook = JSON.parse(bookList[0]);
+            //tworzymy element li z nową książk
+            var newLi = $('<div data-id="' + singleBook.id + '"><span class="bookTitle">' + singleBook.title + '</span><div class="bookDescription" style="margin-top:20px; font-weight: bold; color: blue; font-size: 13px;"></div></div><hr>');
+            //dodajemy element do listy
+            divBooks.append(newLi);
+        }).fail(function () {
+
+        });
+    });
+    
+    //AKTUALIZACJA KSIĄŻEK (PUT)
+    //1) do każdego diva z opisem (lub dodatkowego pod nim) dodajemy formularz edycji książki
+    //2) formualrz jest ukryty domyslnie i zaladowany danymi ksiazki val('wartosc')
+    //3) dodajemy przycisk do edycji analogicznie jak w DELETE
+    //4) zakladamy event na przycisk (pamietamy ze jest ladowany dynamicznie)
+    //5) po kliknieciu pokazujemy formualarz
+    //6) po klikeniciu w submit formularza pobieramy z niego dane (podobnie jak POST)
+    //7) tworzymy obiekt do wyslania (jak w POST) - dodać ID!!!!
+    //8) wysylamy ajaxem metodą PUT przekazujac obiekt (PAMIĘTAMY o dodaniu ID
+    //9) pobieramy z bazy obiekt książki na backendzie z aktualnymi danymi
+    //10) aktualizujemy seterami dane w obiekcie
+    //11) wywołujemy metodę update()
+    //12) jak się uda to w drzewie dom aktualizujemy tytul i opis elementu
+    
+    //USUWANIE KSIĄŻEK (DELETE)
+    //1) dodajemy przycisk obok tytułu z usuwaniem podczas tworzenia ksiazki
+    //pamietamy zeby dodac przycisk jak ladujemy przy ladowaniu strony i dodaniu (POST)
+    //2) zakladamy event na przycisk (pamietamy ze on jest ladowany dynamicznie)
+    //po kliknieciu pobieramy id ksiazki (rodzic) i wysylamy do ajaxa za pomocą
+    //metody DELETE (bardzo podobnie jak GET)
+    //3) pobieracie ksiązkę z bazy odpowiednią metoda po id (filtrujemy na int)
+    //4) mając obiekt ksiązki wywołujemy metodę delete()
+    //5) jeśli się uda to usuwamy książkę z drzewa DOM
+});
